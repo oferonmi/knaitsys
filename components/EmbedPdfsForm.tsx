85fae5f6bb91f7be813cf6fa048ad1fe0558297b
Victor Ocheri <datafilik@gmail.com>
2024-05-04 00:33:23 +0100

    retrieval interface mods

--- components/EmbedPdfsForm.tsx
+++ components/EmbedPdfsForm.tsx
@@ -2,6 +2,7 @@
 
 import { useRef,useState, useEffect, type FormEvent, Dispatch, SetStateAction, useCallback } from "react";
 import { toast } from "react-toastify";
+import { Tooltip } from "flowbite-react";
 import { useDropzone } from "react-dropzone";
 import { CloudUploadIcon } from "@/components/Icons";
 
@@ -84,15 +85,12 @@ export function EmbedPdfsForm(props: {
 
   return (
     <>
-      <form
-        onSubmit={embedPDF}
-        className="flex w-full"
-      >
+      <form onSubmit={embedPDF} className="flex w-full">
         <label
           {...getRootProps({
             htmlFor: "dropzone-file",
             className:
-              "flex flex-col items-center justify-center w-5/6 cursor-pointer bg-white  hover:bg-gray-50 rounded mb-4 mr-8 border border-dashed border-kaito-brand-ash-green",
+              "grow items-center justify-center  cursor-pointer bg-white  hover:bg-gray-50 rounded mb-4 mr-8 border border-dashed border-kaito-brand-ash-green",
           })}
         >
           <div className="flex flex-col items-center justify-center pt-32 pb-36">
@@ -130,34 +128,51 @@ export function EmbedPdfsForm(props: {
           </ul> */}
         </label>
 
-        <button
-          type="submit"
-          className="flex flex-col shrink-0 px-4 py-4 bg-kaito-brand-ash-green text-gray-200 rounded-full h-full max-h-24 w-1/6 max-w-24 items-center"
-        >
-          <div
-            role="status"
-            className={`${isLoading ? "" : "hidden"} flex justify-center`}
+        <Tooltip content="Upload" >
+          <button
+            type="submit"
+            className="flex shrink-0 px-5 py-5 bg-kaito-brand-ash-green text-gray-200 rounded-full max-h-24 max-w-24 items-center"
           >
-            <svg
-              aria-hidden="true"
-              className="w-6 h-6 text-white animate-spin dark:text-white fill-kaito-brand-ash-green"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+            <div
+              role="status"
+              className={`${
+                isLoading ? "" : "hidden"
+              } flex item-center justify-center`}
             >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              <svg
+                aria-hidden="true"
+                className="w-6 h-6 text-white animate-spin dark:text-white fill-kaito-brand-ash-green"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+            <span className={isLoading ? "hidden" : ""}>
+              {/* Upload */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
                 fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-          <span className={isLoading ? "hidden" : ""}>Upload</span>
-        </button>
+                className="bi bi-upload"
+                viewBox="0 0 16 16"
+              >
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z" />
+              </svg>
+            </span>
+          </button>
+        </Tooltip>
       </form>
     </>
   );
