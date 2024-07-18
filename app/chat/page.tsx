ebb3c5c89e5f460a0d1af2088b26193cb3f257e7
Victor Ocheri <datafilik@gmail.com>
2024-07-18 10:48:56 +0100

    Added button to clear current chat thread

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -9,6 +9,7 @@ import { redirect } from "next/navigation";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Tooltip } from "flowbite-react";
 
 const ChatbotPage = () => {
   // sessions
@@ -27,6 +28,7 @@ const ChatbotPage = () => {
   // use OpenAI chat completion
   const {
     messages,
+    setMessages,
     input,
     handleInputChange,
     handleSubmit,
@@ -68,6 +70,18 @@ const ChatbotPage = () => {
 
           <div className="z-10 fixed left-0 right-0 bottom-0 bg-gray-100 bg-opacity-60 border-t-2 border-b-1">
             <div className="container flex max-w-3xl mx-auto my-auto p-5 pt-9 pb-9">
+              <Tooltip content="Clear Chat Thread" className="inline-flex">
+                <button
+                  className="inline-flex bg-kaito-brand-ash-green hover:bg-kaito-brand-ash-green items-center font-medium text-gray-200 rounded-full px-5 py-5 mr-2"
+                  type="button"
+                  onClick={() => {setMessages([]);}}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                  </svg>
+                </button>
+              </Tooltip>
+
               {/* <label className="text-black" htmlFor="llm-selector">Select LLM: </label> */}
               <select
                 onChange={handleLlmApiChange}
--- components/RetrievalChatWindow.tsx
+++ components/RetrievalChatWindow.tsx
@@ -324,6 +324,18 @@ export function RetrievalChatWindow(props: {
             id="chat-form"
           >
             <div className="flex w-full m-auto gap-2">
+              <Tooltip content="Clear Chat Thread" className="inline-flex">
+                <button
+                  className="inline-flex bg-kaito-brand-ash-green hover:bg-kaito-brand-ash-green items-center font-medium text-gray-200 rounded-full px-5 py-5 mr-2"
+                  type="button"
+                  onClick={() => {setMessages([]);}}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                  </svg>
+                </button>
+              </Tooltip>
+
               <Tooltip content="Upload Corpus" className="inline-flex">
                 <button
                   className=" px-5 py-5 bg-kaito-brand-ash-green text-gray-200 rounded-full ml-2"
