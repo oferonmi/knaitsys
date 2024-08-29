56e3e81261e89f226f640c792be9e1ef8f065d65
oferonmi <datafilik@gmail.com>
2024-08-30 00:02:49 +0100

    Added image input to LLM feature

---
+++ app/multimodal/layout.tsx
@@ -0,0 +1,34 @@
+import { Inter } from "next/font/google";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+
+const inter = Inter({ subsets: ['latin'], weight: '300', })
+
+export const metadata = {
+  title: "Kaitosys - Multimodal Bot",
+  description: "Multimodal AI Chatbot ",
+};
+
+export default function MutlimodalBotLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-gradient-to-r from-teal-100 to-teal-100`}
+      >
+        <Header
+          menu={[
+            { id: 1, value: "Text", url: "/text" },
+            { id: 2, value: "Audio", url: "/audio" },
+            { id: 3, value: "Code", url: "/code" },
+          ]}
+        />
+        {children}
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
+}
---
+++ app/multimodal/page.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { useChat } from 'ai/react';
+import { useRef, useState } from 'react';
+
+export default function Chat() {
+    //LLM engine API route
+    const [llmApiRoute, setLlmApiRoute] = useState("/api/multimodal/chat/openai");
+    const { messages, input, handleInputChange, handleSubmit } = useChat({api: llmApiRoute,});
+
+    const [files, setFiles] = useState<FileList | undefined>(undefined);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    return (
+        <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+            {messages.map(m => (
+                <div key={m.id} className="whitespace-pre-wrap text-black">
+                    {m.role === 'user' ? 'User: ' : 'AI: '}
+                    {m.content}
+                    <div>
+                        {m?.experimental_attachments?.filter(
+                            attachment => attachment?.contentType?.startsWith('image/'),
+                        )
+                        .map((attachment, index) => (
+                            <img
+                                key={`${m.id}-${index}`}
+                                src={attachment.url}
+                                width={500}
+                                alt={attachment.name}
+                            />
+                        ))}
+                    </div>
+                </div>
+            ))}
+        
+            <form
+                className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl space-y-2 text-black"
+                onSubmit={event => {
+                    handleSubmit(event, {
+                        experimental_attachments: files,
+                    });
+                    
+                    setFiles(undefined);
+                    
+                    if (fileInputRef.current) {
+                        fileInputRef.current.value = '';
+                    }
+                }}
+            >
+                <input
+                    type="file"
+                    className=""
+                    onChange={event => {
+                        if (event.target.files) {
+                            setFiles(event.target.files);
+                        }
+                    }}
+                    multiple
+                    ref={fileInputRef}
+                />
+                <input
+                    className="w-full p-2"
+                    value={input}
+                    placeholder="Say something..."
+                    onChange={handleInputChange}
+                />
+            </form>
+        </div>
+    );
+}
