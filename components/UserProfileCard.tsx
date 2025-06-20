cc014883fa831877154bf3da732ab9bcaa1ceb82
Victor Ocheri <datafilik@gmail.com>
2025-06-20 19:17:36 +0100

    Update default avatar path in UserProfileCard component

--- components/UserProfileCard.tsx
+++ components/UserProfileCard.tsx
@@ -10,17 +10,21 @@ interface UserProfileCardProps {
     provider?: string;
 }
 
-const UserProfileCard: React.FC<UserProfileCardProps> = ({ name, email, image, provider }) => (
+const UserProfileCard: React.FC<UserProfileCardProps> = ({
+    name,
+    email,
+    image,
+    provider,
+}) => (
     <div className="bg-white dark:bg-gray-900 rounded-lg shadow p-6 flex flex-col items-center">
-        
         <Image
             width={96}
             height={96}
-            src={ image ? image : "/avatars/default_avatar_2.jpeg" } 
+            src={image ? image : "/avatars/default_user_avatar.JPG"}
             alt="User avatar"
             className="w-24 h-24 rounded-full mb-4 border-2 border-gray-300 dark:border-gray-700"
         />
-       
+
         <h2 className="text-xl font-semibold mb-1">{name}</h2>
         <p className="text-gray-600 dark:text-gray-300 mb-1">{email}</p>
         {provider && (
