99fbf3beba43dd00371cd7ac103a3e93da460864
Victor Ocheri <datafilik@gmail.com>
2025-08-13 16:56:13 +0100

    Refactor ZhiPu route to streamline model usage and enhance debugging output

--- app/api/chat/remote_chat/zhipu/route.ts
+++ app/api/chat/remote_chat/zhipu/route.ts
@@ -2,21 +2,32 @@ import { createOpenAI } from '@ai-sdk/openai';
 import { convertToCoreMessages, streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+// export const maxDuration = 30;
 
-const zai = createOpenAI({
-    // custom settings
-    baseURL: "https://api.z.ai/api/paas/v4/",
-    apiKey: process.env.ZHIPU_API_KEY ?? '',
-})
+// const zai = createOpenAI({
+//     // custom settings
+//     baseURL: "https://api.z.ai/api/paas/v4/",
+//     apiKey: process.env.ZHIPU_API_KEY ?? '',
+// })
 
 export async function POST(req: Request) {
     const { messages } = await req.json();
 
     const result = streamText({
-        model: zai('glm-4.5'),
+        model: "zai/glm-4.5",
         messages: convertToCoreMessages(messages),
+        providerOptions: {
+            zai: {
+                thinking: {
+                    type: 'disabled',
+                }
+            }
+        }
     } as any);
+
+    // Debugging output. comment out in production
+    const text = await result.text
+    console.log("Zhipu result", text);
 
     return result.toDataStreamResponse();
 }
